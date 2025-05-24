ebea23686c68395f3eebe69a9f41f5966040283b
Benjamín <benjamin.diaze@outlook.com>
2025-05-23 20:49:43 -0400

    Se añade despliegue en region secundaria

--- src/BDiazECertificate/Program.cs
+++ src/BDiazECertificate/Program.cs
@@ -9,37 +9,29 @@ namespace BDiazECertificate
     {
         public static void Main(string[] args)
         {
-            string appName = System.Environment.GetEnvironmentVariable("APP_NAME")!;
-            string account = System.Environment.GetEnvironmentVariable("ACCOUNT_AWS")!;
-            string region = System.Environment.GetEnvironmentVariable("REGION_AWS")!;
+            string appName = System.Environment.GetEnvironmentVariable("APP_NAME") ?? throw new ArgumentNullException("APP_NAME");
+            string account = System.Environment.GetEnvironmentVariable("ACCOUNT_AWS") ?? throw new ArgumentNullException("ACCOUNT_AWS");
+            string region = System.Environment.GetEnvironmentVariable("REGION_AWS") ?? throw new ArgumentNullException("REGION_AWS");
+            string secondaryRegion = System.Environment.GetEnvironmentVariable("SECONDARY_REGION_AWS") ?? throw new ArgumentNullException("SECONDARY_REGION_AWS");
 
             var app = new App();
-            new BDiazECertificateStack(app, $"Cdk{appName}Certificate", new StackProps
-            {
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                Env = new Amazon.CDK.Environment
-                {
+            // Despliegue en región primaria...
+            _ = new BDiazECertificateStack(app, $"Cdk{appName}Certificate", new StackProps {
+                Env = new Amazon.CDK.Environment {
                     Account = account,
                     Region = region,
                 }
-
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                Env = new Amazon.CDK.Environment
-                {
-                    Account = "123456789012",
-                    Region = "us-east-1",
-                }
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
             });
+
+            // Despliegue en región secundaria...
+            _ = new BDiazECertificateStack(app, $"Cdk{appName}Certificate", new StackProps {
+                Env = new Amazon.CDK.Environment {
+                    Account = account,
+                    Region = secondaryRegion,
+                }
+            });
+
             app.Synth();
         }
     }
