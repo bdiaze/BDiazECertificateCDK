650287e66c059a6a192b5e07a1254cd1da903443
Benjamín <benjamin.diaze@outlook.com>
2025-05-23 21:02:37 -0400

    Se cambia despliegue de region secundaria a receta

--- src/BDiazECertificate/Program.cs
+++ src/BDiazECertificate/Program.cs
@@ -12,23 +12,13 @@ namespace BDiazECertificate
             string appName = System.Environment.GetEnvironmentVariable("APP_NAME") ?? throw new ArgumentNullException("APP_NAME");
             string account = System.Environment.GetEnvironmentVariable("ACCOUNT_AWS") ?? throw new ArgumentNullException("ACCOUNT_AWS");
             string region = System.Environment.GetEnvironmentVariable("REGION_AWS") ?? throw new ArgumentNullException("REGION_AWS");
-            string secondaryRegion = System.Environment.GetEnvironmentVariable("SECONDARY_REGION_AWS") ?? throw new ArgumentNullException("SECONDARY_REGION_AWS");
 
             var app = new App();
 
-            // Despliegue en región primaria...
             _ = new BDiazECertificateStack(app, $"Cdk{appName}Certificate", new StackProps {
                 Env = new Amazon.CDK.Environment {
                     Account = account,
                     Region = region,
-                }
-            });
-
-            // Despliegue en región secundaria...
-            _ = new BDiazECertificateStack(app, $"Cdk{appName}Certificate", new StackProps {
-                Env = new Amazon.CDK.Environment {
-                    Account = account,
-                    Region = secondaryRegion,
                 }
             });
 
